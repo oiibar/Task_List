e13eb92e287228477cb71e8f409795f82582a1d4
oiibar <aibartolegenov.2283@gmail.com>
2022-12-01 23:06:37 +0600

    first commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Todo from './Todo'
+
+export default function TodoList({ todos, setTodos, filteredTodos }) {
+	return (
+		<div className="todo-container">
+      		<ul className="todo-list">
+				{filteredTodos.map(todo => (
+					<Todo
+						todos={todos}
+						setTodos={setTodos}
+						text={todo.text}
+						key={todo.id}
+						todo={todo}
+					/>
+				))}
+			</ul>
+    	</div>
+	)
+}
